f3c381afcc049304a68b76ff28c273bd63c9dfd2
SidharthMN <sidharthnisha7777@gmail.com>
2026-06-17 12:45:56 +0530

    Upto custom module and implementation

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def addition (a,b):
+    return a+b
+
+def substraction (a,b):
+    return a-b
+
+def multiplication (a,b):
+    return a*b
+
+def division (a,b):
+    return a/b
+
+if __name__ == "__main__":
+    print("Calculator module executed directly")
---
+++ calculator_implementation.py
@@ -0,0 +1,6 @@
+import calculator
+
+print(calculator.addition(5,10))
+print(calculator.substraction(10,5))
+print(calculator.multiplication(10,5))
+print(calculator.division(10,5))
