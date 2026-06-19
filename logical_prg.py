82855b1e600828cb0187f9e4d36edbe6f6ad5a6d
SidharthMN <sidharthnisha7777@gmail.com>
2026-06-19 13:05:47 +0530

    logical programs pt .2

--- logical_prg.py
+++ logical_prg.py
@@ -1,3 +1,4 @@
+'''
 #Prime number
 
 number = int(input("Enter a number : "))
@@ -13,7 +14,8 @@ else:
        print("It is a prime number")
     else:
        print("Not a prime number")   
-
+'''
+'''
 #fibonacci series
 
 
@@ -26,3 +28,75 @@ for i in range(n):
    c = a+b
    a = b
    b = c
+'''
+'''
+#palindrome
+
+number = int(input("Enter a number : "))
+temp =number
+reverse = 0
+while number > 0:
+   remainder = number%10
+   reverse = reverse*10+remainder
+   number = number//10
+
+if temp == reverse:
+   print("Number is Palindrome")
+else:
+   print("Not a palindrome")
+
+#Palindrome - String
+
+str = input("Enter a string : ")
+rev = ""
+
+for i in str:
+   rev = i + rev
+
+if str == rev:
+   print(f"{rev} is a Palindrome")
+else:
+   print("Not a palindrome")
+
+'''
+'''
+#count the number of digits
+number = int(input("Enter a number : "))
+count = 0
+temp = abs(number)
+
+if temp == 0:
+   count = 1
+else:
+   while temp > 0:
+     temp = temp//10
+     count+=1
+
+print("Count is :",count)
+'''
+'''
+#Sum of digits
+number = int(input("Enter a number : "))
+sum_digits = 0
+temp = abs(number)
+
+while temp > 0:
+   digit = temp%10
+   sum_digits += digit
+   temp = temp//10
+
+print("Sum of digits : ",sum_digits)
+'''
+'''
+#Product of digits
+number = int(input("Enter a number : "))
+product_digits = 0
+temp = abs(number)
+
+while temp > 0:
+   digit = temp%10
+   product_digits *= digit
+   temp = temp//10
+
+print("Product of digits : ",product_digits)
+'''
