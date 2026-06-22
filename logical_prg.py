4853fbfacc1b130c0a3f0f3945821b83a1d9c0cd
SidharthMN <sidharthnisha7777@gmail.com>
2026-06-22 13:30:19 +0530

    logical prg upto smallest and largest

--- logical_prg.py
+++ logical_prg.py
@@ -100,9 +100,8 @@ while temp > 0:
 
 print("Product of digits : ",product_digits)
 '''
-
-pin = int(input("Enter the pin :"))
-welcome = "Welcome User"
+'''
+#ATM 
 correct_pin = 2255
 balance =100000
 attempts = 0
@@ -110,7 +109,96 @@ attempts = 0
 while attempts<3:
    pin = int(input("Enter the pin :"))
    if pin == correct_pin:
-      print(welcome)
+      print("Login Successfull")
+      while True:
+         print("\n1.Balance Check")
+         print("2.Withdraw")
+         print("3.Deposit")
+         print("4.Exit")
+         choice = int(input("Enter a choice : "))
+         if choice == 1:
+            print("Balance: ",balance)
+         elif choice == 2:
+            amount = int(input("Enter the withdrawal amount : "))
+            if amount <= balance:
+               balance -= amount
+            else:
+               print("Insufficient balance")
+         elif choice == 3:
+            amount = int(input("Enter the amount to be deposited : "))
+            balance += amount
+            print("Amount deposited successfully.")
+         elif choice == 4:
+            print("Thank you")
+            break
+         else:
+            print("Invalid choice")
+      break
    else:
-      print("Try again later")
-   
+      attempts += 1
+      print("Incorrect PIN")
+else:
+   print("Account blocked")
+'''
+'''
+#Seat Availability System
+available_seats = 35
+
+while available_seats > 0:
+   print("Available Seats are : ",available_seats)
+   book_seats = int(input("Enter the number of seats to be booked : "))
+   if book_seats <= available_seats:
+      available_seats -= book_seats
+      print(f"{book_seats} seats are booked successfully")
+      remaining_seats = available_seats
+      print("Remaining Seats are : ",remaining_seats)
+   else:
+      print("Seats are not available")
+   choice = input("Do you want to continue (Yes/No): ").lower()
+   if choice == "no":
+      break
+print("Booking closed")
+'''
+'''
+#Fizzbuzz patern
+start_value = int(input("Enter the value to start : "))
+end_value = int(input("Enter the value to end : "))
+for num in range(start_value,end_value+1):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num% 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+'''
+'''
+#find the largest and smallest number in a list
+numbers = []
+n = int(input("Enter the numbers to add in list : "))
+for i in range (n):
+   num = int(input(f"Enter number {i+1}: "))
+   numbers.append(num)
+largest=numbers[0]
+smallest=numbers[0]
+for num in numbers:
+   if num > largest:
+      largest = num
+   if num < smallest:
+      smallest = num
+print("Largest of given number : ",largest)
+print("Smallest of given number : ",smallest)
+'''
+'''
+#10,15,21,3,8
+largest=10
+smallest=10
+num=10
+num=15
+10>15 False
+10 < 15
+largest =15
+num =21
+21 > 10
+'''
